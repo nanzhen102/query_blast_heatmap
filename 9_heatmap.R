45b70df0cf04c58a8f757e287b419b21be02ae5b
Nanzhen Qiao <nanzhen.qiao@gmail.com>
2022-03-24 13:55:43 -0600

    update R script - edit text

--- 9_heatmap.R
+++ 9_heatmap.R
@@ -1,12 +1,17 @@
-data <- read.csv('/Volumes/USB/1_phd_2020/5_doctoral_program/0.1_type_strain_tree/5_weissella_tree_0921/4_add_two_new_genomes/3_query_blast/2_multi_query/75cov_50aa_proportation_Liquoricoccoides_plus_337genomes_etc_fullname.csv', header = TRUE)
+data <- read.csv('/Users/nanzhen/Desktop/GitHub/Heatmap_axis_reorder/75cov_50aa_proportation_Liquoricoccoides_plus_337genomes_etc_reorder_full_2.csv', header = TRUE)
 
 data$Genus<-factor(data$Genus, levels=data$Genus[33:1])
+
+# install.packages("reshape2") 
+# install.packages("ggplot2")
 
 library(reshape2)
 melted_cormat <- melt(data)
 library(ggplot2)
 
-plot1 <- ggplot(melted_cormat, aes(x=variable, y=Genus, fill=value)) + geom_tile() + scale_fill_gradient2(low = "white", high = "#FF0000", space = "Lab", name = "Proportion\nof genes\nin genera") + theme(axis.text.x = element_text(angle = 45, vjust = 1, size = 25, hjust = 1), axis.text.y = element_text(face = "italic", size = 25)) + coord_fixed()
-plot2 <- plot1 + xlab("Genes") + ylab("Genera")
-plot2
+plot1 <- ggplot(melted_cormat, aes(x=variable, y=Genus, fill=value)) + geom_tile() + scale_fill_gradient2(low = "white", high = "#FF0000", space = "Lab", name = "Proportion\nof genes\nin genera") + theme(axis.text.x = element_text(angle = 45, vjust = 1, size = 30, hjust = 1), axis.text.y = element_text(face = "italic", size = 30), legend.text = element_text(size = 30), legend.key.size = unit(2, 'cm'), legend.title = element_text(size = 30)) + coord_fixed()
+plot2 <- plot1 + xlab("Genes") + ylab("Genera") + theme(axis.title.x=element_text(size=30), axis.title.y=element_text(size=30))
+# plot2
+
+ggsave("dddd.png", units="in", width=30, height=40, dpi=300)
 
