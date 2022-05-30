7f38fef714fd87072803a6ecc8186e83c7d4fe0f
Nanzhen Qiao <nanzhen.qiao@gmail.com>
2022-05-30 00:20:40 -0600

    for dup hits

---
+++ 2_dup_hits/4_data_filter.py
@@ -0,0 +1,14 @@
+# -*-coding:utf-8 -*-
+import pandas as pd
+import numpy as np
+
+input_file = pd.DataFrame(pd.read_csv("all_prot_query_screened_header.csv", header = 0))
+
+# print(input_file.keys())
+
+mask=((input_file["identity"]>=40) & (input_file["query coverage"]>= 70))
+
+input_file=input_file.loc[mask] #还不知道为什么需要加这一句
+
+input_file.to_csv("all_prot_query_screened_header_40ide_70cov.csv", index = False) 
+
---
+++ 2_dup_hits/5_positive_negative.py
@@ -0,0 +1,25 @@
+# -*-coding:utf-8 -*-
+#!/bin/python3
+
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+
+strain_query = defaultdict(list)
+query_list = []
+csv_infile = open('all_prot_query_screened_header_40ide_70cov.csv')
+
+next(csv_infile) # 这一步不能少，否则会多出一列
+
+for line in csv_infile:
+    query, strain, *other = line.strip().split(',')
+    strain = '_'.join(strain.split('_')[0:-1])
+    query_list.append(query) if query not in query_list else ''
+    strain_query[strain].append(query) if query not in strain_query[strain] else ''
+
+tmp_matrix = np.full((len(strain_query), len(query_list)), 0) # negetive be -1 or 0
+out_data = pd.DataFrame(tmp_matrix, columns=query_list, index=list(strain_query.keys()))
+for strain in strain_query:
+    for query in strain_query[strain]:
+        out_data.loc[strain, query] = 1
+out_data.to_csv('all_prot_query_screened_header_40ide_70cov_positive_negative.csv')
