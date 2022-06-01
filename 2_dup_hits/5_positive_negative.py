c64091b12f6c76b8ad28c2aa4a02f41606921ca9
Nanzhen Qiao <nanzhen.qiao@gmail.com>
2022-06-01 11:30:32 -0600

    bb

--- 2_dup_hits/5_positive_negative.py
+++ 2_dup_hits/5_positive_negative.py
@@ -17,12 +17,18 @@ next(csv_infile) # 这一步不能少，否则会多出一列
 for line in csv_infile:
     query, strain_seq, *other = line.strip().split(',') # the column for query and strain_seq
     strain = '_'.join(strain_seq.split('_')[0:-1]) # the strain name
-    query_list.append(query) if query not in query_list else '' # get all query name without duplicates
-    strain_list.append(strain) if strain not in strain_list else ''
-    strain_seq_list.append(strain_seq) if strain_seq not in strain_seq_list else ''
+    print(strain)
     
-    strain_query[strain_seq].append(query) if query not in strain_query[strain] else ''
-    # print(strain_query[strain])
+    # 做一步，print一下。
+    # 
+
+
+    # query_list.append(query) if query not in query_list else '' # get all query name without duplicates
+    # strain_list.append(strain) if strain not in strain_list else ''
+    # strain_seq_list.append(strain_seq) if strain_seq not in strain_seq_list else ''
+    
+    # strain_query[strain_seq].append(query) if query not in strain_query[strain] else ''
+    # # print(strain_query[strain])
 
 # tmp_matrix = np.full((len(strain_query), len(query_list)), 0) # negetive be -1 or 0
 # out_data = pd.DataFrame(tmp_matrix, columns=query_list, index=list(strain_query.keys()))
