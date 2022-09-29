14955cbf2caa2d5e9f429bb6b1a7e864ad563d6a
Nanzhen Qiao <nanzhen.qiao@gmail.com>
2022-09-29 09:19:30 -0600

    dd

--- backup_run_script/6_species_match_canRun.py
+++ backup_run_script/6_species_match_canRun.py
@@ -4,22 +4,20 @@ import pandas as pd
 from re import search
 
 input_partial_fname = 'all_prot_query_screened_header_40ide_70cov_positive_negative.csv'
-input_full_fname = './full_name_Sep26_2022.csv'
-# input_gene_df = pd.read_csv("../query_info.csv", header = 0, index_col = 0) # open query_gene.csv
+input_full_fname = 'full_name_Sep26_2022.csv'
 
 output_fname = input_partial_fname.split(".")[0]  + "_species_matched.csv"
 
 input_partial_df = pd.read_csv(input_partial_fname , header=None, index_col = False) # open file with incomplete names
-input_full_df = pd.read_csv(input_full_fname , header=None, index_col = False) # open file with complete (full) names
+input_full_df = pd.read_csv(input_full_fname , header=0, index_col = 0) # open file with complete (full) names
+
+# print(input_full_df)
 
 
 part_df_row_num, part_df_col_num = input_partial_df.shape # rows - columns 
 ful_df_row_num, ful_df_col_num = input_full_df.shape # rows - columns 
-# gene_df_row_num, gene_df_col_num = input_gene_df.shape
 
 input_partial_df.insert(loc=1, column="species", value="not_nan") # insert a column - to write the matched species name
-input_partial_df.insert(loc=2, column="sum_up", value=1) # insert a column - to calculate the proportion
-# print(input_partial_df)
 
 if pd.isna(input_partial_df.iloc[0,0]) == True: # in pos_neg csv, the first column of the first row is nan, thus need to fill a value
     input_partial_df.iloc[0,0] = "not_nan"
@@ -31,13 +29,4 @@ for par_row in range(part_df_row_num):
         if search(part_target,full_target): # if two ACC match	
             input_partial_df.iloc[par_row,1] = input_full_df.iloc[ful_row,1]
 
-# for par_col in range(3, part_df_col_num + 2): # the first cells are Nan, which causes error; two columns were added by the former command lines, thus the range of columns need to be enlarged (+2)
-# 	ACC_target = input_partial_df.iloc[0, par_col] # the first row is ACC of genes
-# 	for gene_row in range(gene_df_row_num):
-# 		gene_target = input_gene_df.iloc[gene_row,0] # the first column is names of genes
-# 		if search(ACC_target,gene_target):
-# 			input_partial_df.iloc[0, par_col] = input_gene_df.iloc[gene_row,1] # replace ACC with gene names
-
-input_partial_df.to_csv(output_fname, index_label = False, header = None)
-            
-    
+input_partial_df.to_csv(output_fname, index_label = 0, header = 0)
--- backup_run_script/7_group.py
+++ backup_run_script/7_group.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-in_df = pd.read_csv('all_prot_query_screened_header_40ide_70cov_positive_negative_species_gene_matched_cal_edited.csv', header = [0], index_col = 0)
+in_df = pd.read_csv('all_prot_query_screened_header_40ide_70cov_positive_negative_species_matched.csv', header = [0], index_col = 0)
 
 r_num, co_num = in_df.shape
 
@@ -14,6 +14,6 @@ for r in range(r_num):
 in_df.insert(0,"genus", genus_col, True)
 in_df_grouped = in_df.groupby("genus")
 
-in_df_grouped.sum().reset_index().to_csv('all_prot_query_screened_header_40ide_70cov_positive_negative_species_gene_matched_cal_edited_grouped.csv')
+in_df_grouped.sum().reset_index().to_csv('all_prot_query_screened_header_40ide_70cov_positive_negative_species_matched_grouped.csv')
 
     
