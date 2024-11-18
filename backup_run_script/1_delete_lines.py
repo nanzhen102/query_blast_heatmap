587db3287a0a1e39a4a80df274f0ca6bbdcd702f
Nanzhen Qiao <nanzhen.qiao@gmail.com>
2024-11-18 15:48:56 -0700

    new genera

--- backup_run_script/1_delete_lines.py
+++ backup_run_script/1_delete_lines.py
@@ -1,6 +1,6 @@
 # 删除注释行，即#开始的行。且不留空行
 
-with open ("inlA_query_blast.txt", "r") as input:
+with open ("/Users/nanzhen/Documents/1_phd_2020/5_doctoral_program/0.6_metabolism_taxonomy_202405/7_fig1_6/fig3_update_v6/query_blast_combine.txt", "r") as input:
   lines = input.readlines()
 with open ("all_prot_query_screened.txt", "w") as output:
   for line in lines:
