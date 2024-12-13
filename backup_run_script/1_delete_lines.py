62a75efd54fbbc3baa873c1827ddadd1fec7b962
Nanzhen Qiao <nanzhen.qiao@gmail.com>
2024-12-13 15:04:30 -0700

    change order

--- backup_run_script/1_delete_lines.py
+++ backup_run_script/1_delete_lines.py
@@ -1,6 +1,6 @@
 # 删除注释行，即#开始的行。且不留空行
 
-with open ("/Users/nanzhen/Documents/1_phd_2020/5_doctoral_program/0.6_metabolism_taxonomy_202405/7_fig1_6/fig3_update_v6/query_blast_combine.txt", "r") as input:
+with open ("/Users/nanzhen/Documents/1_phd_2020/5_doctoral_program/0.6_metabolism_taxonomy_202405/8_fig1_7_202411/fig1_no4_Xylo_5_10_11/query_blast_combined.txt", "r") as input:
   lines = input.readlines()
 with open ("all_prot_query_screened.txt", "w") as output:
   for line in lines:
