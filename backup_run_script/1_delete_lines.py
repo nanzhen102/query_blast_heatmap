b491188da6839f6f0c85ae93fa68fb1baee777ef
Nanzhen Qiao <nanzhen.qiao@gmail.com>
2025-01-02 17:28:51 -0700

    remove

--- backup_run_script/1_delete_lines.py
+++ backup_run_script/1_delete_lines.py
@@ -1,6 +1,6 @@
 # 删除注释行，即#开始的行。且不留空行
 
-with open ("/Users/nanzhen/Documents/1_phd_2020/5_doctoral_program/0.6_metabolism_taxonomy_202405/8_fig1_7_202411/fig1_no4_Xylo_5_10_11/query_blast_combined.txt", "r") as input:
+with open ("/Users/nanzhen/Documents/1_phd_2020/5_doctoral_program/0.6_metabolism_taxonomy_202405/9_fig1_7_202412_again/fig7_3rd/query_blast_combined.txt", "r") as input:
   lines = input.readlines()
 with open ("all_prot_query_screened.txt", "w") as output:
   for line in lines:
